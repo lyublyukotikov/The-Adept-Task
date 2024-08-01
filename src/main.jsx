ffa459d7add58cedd6df978a8317cbf64e758d7b
Алексей <karpenochkanv@gmail.com>
2024-08-01 22:11:27 +0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import App from "./App.jsx";
 import "./styles/styles.scss";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/index.ts"; // Убедитесь, что импортируете store из правильного файла
-
+import store from "./store/index.ts"; 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
