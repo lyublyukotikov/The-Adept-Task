e6ba1d5645cc6be9337f7068e8f98c0986166cb3
Алексей <karpenochkanv@gmail.com>
2024-08-17 13:19:37 +0300

    Добавлены пропущенные JS и CSS файлы

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-// index.js
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/styles.scss";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store/index.ts"; 
+import store from "./store/index.ts";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
+  <HashRouter>
+    <React.StrictMode>
+      <Provider store={store}>
         <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>
+      </Provider>
+    </React.StrictMode>
+  </HashRouter>
 );
